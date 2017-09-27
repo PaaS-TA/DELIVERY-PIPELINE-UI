297cc40a90518aeb50195c8878be85d507b330b2
REX <rexx4314@gmail.com>
2017-09-27 13:28:12 +0900

    modify siteMesh config

--- src/main/java/paasta/delivery/pipeline/ui/config/WebSiteMeshFilter.java
+++ src/main/java/paasta/delivery/pipeline/ui/config/WebSiteMeshFilter.java
@@ -15,8 +15,9 @@ public class WebSiteMeshFilter extends ConfigurableSiteMeshFilter {
 
     @Override
     protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
-        builder.addDecoratorPath("/*", "/WEB-INF/jsp/sitemesh/defaultDecorator.jsp")
-                .addExcludedPath("/common/error/*")
+        builder.addDecoratorPath("/common/error/*", "/WEB-INF/jsp/sitemesh/errorDecorator.jsp")
+                .addDecoratorPath("/dashboard/*", "/WEB-INF/jsp/sitemesh/defaultDecorator.jsp")
+                .setIncludeErrorPages(true)
                 .addExcludedPath("/admin/index");
     }
 
